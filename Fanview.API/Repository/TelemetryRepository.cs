b2c7b96219714bebf12b8691c602f1812d64d12b
Vinoth <vinothkummar@hotmail.com>
2018-06-04 22:50:41 +0100

    Adding the Assest to the FanView Api; Adding new repository to read the data from mongodb collections

---
+++ Fanview.API/Repository/TelemetryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fanview.API.Repository;
+using Fanview.API.Repository.Interface;
+using FanviewPollingService.Model;
+using FanviewPollingService.Contracts;
+
+namespace Fanview.API.Repository
+{
+    public class TelemetryRepository : ITelemetryRepository
+    {
+        public Task<IEnumerable<PlayerKill>> GetPlayerKills()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FanviewPollingService/Utility/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace FanviewPollingService.Utility
+{
+    public static class Util
+    {
+        public static DateTime ToDateTimeFormat(this string lastEventTimeStamp)
+        {
+            DateTimeFormatInfo dateFormatProvider = new DateTimeFormatInfo();
+            dateFormatProvider.LongDatePattern = "MM/dd/yy HH:mm:ss";
+            return DateTime.Parse(lastEventTimeStamp, dateFormatProvider);
+        }
+    }
+}
