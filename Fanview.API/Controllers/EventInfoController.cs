9cbf782ccfffa357f3dc34118d68f7a5bf417075
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2018-07-02 00:14:34 +0100

    Added a new Api EventInfoController

---
+++ Fanview.API/Controllers/EventInfoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Fanview.API.Model;
+
+namespace Fanview.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventInfoController : ControllerBase
+    {
+        // GET: api/EventInfo
+        [HttpGet("Location")]
+        public EventLocation Get()
+        {
+            var location = new EventLocation();
+            location.EventName = "PUBG_Global_Invitational 2018";
+            location.EventPlace = "Mercedes-Benz Arena";
+            location.Address1 = "Mercedes-Platz 1";
+            location.PostCode = " 10243";
+            location.State = "Berlin";
+            location.Country = "Germany";
+
+            return location;
+
+        }
+    }
+}
