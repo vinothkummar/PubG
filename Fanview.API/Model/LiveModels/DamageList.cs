4aec1c96a81c9230f30b4561cb4ba8ee1eeeaf90
trobertsson <tomas.robertsson@fanview.co>
2018-07-12 16:50:03 +0100

    two property was added to damagelist

--- Fanview.API/Model/LiveModels/DamageList.cs
+++ Fanview.API/Model/LiveModels/DamageList.cs
@@ -1,4 +1,6 @@
-﻿namespace Fanview.API.Model.LiveModels
+﻿using System;
+
+namespace Fanview.API.Model.LiveModels
 {
     public class DamageList
     {
@@ -9,6 +11,8 @@
         public int PlayerId { get; set; }
         public int kills { get; set; }
         public double DamageDealt { get; set; }
+        public int TeamId { get; set; }
+        public TimeSpan SurvivlTime { get; set; }
 
     }
 }
