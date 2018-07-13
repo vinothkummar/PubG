8a36c32b990821d9b2a8e98228e53c2a5ef3775b
trobertsson <tomas.robertsson@fanview.co>
2018-07-13 10:59:38 +0100

    the ILivekiller class were added

--- Fanview.API/Model/LiveModels/KillZone.cs
+++ Fanview.API/Model/LiveModels/KillZone.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Fanview.API.Model.LiveModels
 {
     public class KillZone
     {
         public string MatchName { get; set; }
         public int MatchId { get; set; }
-        public int[] kills { get; set; }
-        public string KillerName { get; set; }
-        public Livelocation Location { get; set; }
+        public IEnumerable<LiveKiller> killers { get; set; }
     }
 }
