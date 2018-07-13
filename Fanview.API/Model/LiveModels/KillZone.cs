6e2f848c59eff4db78e9abd3c9e04bd9327ac88e
trobertsson <tomas.robertsson@fanview.co>
2018-07-13 13:22:51 +0100

    killzone class changed

--- Fanview.API/Model/LiveModels/KillZone.cs
+++ Fanview.API/Model/LiveModels/KillZone.cs
@@ -9,7 +9,7 @@ namespace Fanview.API.Model.LiveModels
     public class KillZone
     {
         public string MatchName { get; set; }
-        public int MatchId { get; set; }
+        public string MatchId { get; set; }
         public string KillerName { get; set; }
         public int KillerId { get; set; }
         public int TeamId { get; set; }
