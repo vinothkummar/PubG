9faf717f4c88cf1b185b061944a4a2f378d771bb
trobertsson <tomas.robertsson@fanview.co>
2018-07-17 10:44:13 +0100

    changed the distinct method

--- Fanview.API/Repository/TeamPlayerRepository.cs
+++ Fanview.API/Repository/TeamPlayerRepository.cs
@@ -44,20 +44,38 @@ namespace Fanview.API.Repository
         //I'm changing this part of code
         public async Task<IEnumerable<TeamPlayer>> GetTeamPlayers()
         {
-            //var teamplayers = await _genericTeamPlayerRepository.GetAll("TeamPlayers");
-            //var list = teamplayers.ToList();
-            //var playername = teamplayers.Select(x => x.PlayerName).ToList();
-            //var distinct = teamplayers.Select(x => x.PlayerName).Distinct().ToList();
-            //var unique = teamplayers.DistinctBy(x =>new { x.PlayerName, x.PlayerStatus, x.PubgAccountId }).ToList();
-            ////var unique = teamplayers.GroupBy(t => new { t.PlayerName, t.Id, t.MatchId, t.PubgAccountId }).Select(g => g.First()).ToList();
-            //return unique;
-            return null;
+            var teamplayers = await _genericTeamPlayerRepository.GetAll("TeamPlayers");
+            var list = teamplayers.ToList();
+            var mydistinc = teamplayers.GroupBy(o => new { o.PlayerName, o.PubgAccountId }).Select(o => o.FirstOrDefault()).ToList();
+            return mydistinc;
+
         }
         ////I'm changing this part of code 
         public async Task<TeamLineUp> GetTeamandPlayers()
         {
-
-
+            var teamPlayers = await _genericTeamPlayerRepository.GetAll("TeamPlayers");
+            var teamplayerlist=teamPlayers.ToList();
+            var teams = await _gebericTeamRepository.GetAll("Team");
+            var teamlist = teams.ToList();
+            var query =teamlist.GroupJoin(teamplayerlist, tp => tp.Id, t => t.TeamId, (t, tp) => new
+            {
+                TeamId = t.Id,
+                TeamName = t.Name,
+                TeamPlayers = tp.Select(s => s.PlayerName)
+            });
+            var teamlineup = new TeamLineUp();
+            var teamlineplayers = new List<TeamLineUpPlayers>();
+            foreach(var teamplayer in query)
+            {
+                teamlineup.TeamId = teamplayer.TeamId;
+                teamlineup.TeamName = teamplayer.TeamName;
+                foreach(var player in teamplayer.TeamPlayers)
+                {
+                    teamlineplayers.Add(new TeamLineUpPlayers { PlayerName = player });
+                }
+                teamlineup.TeamPlayer = teamlineplayers;
+            }
+            var result = teamlineup;
             return null;
 
         }
