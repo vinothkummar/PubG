d9ad9183fb648b2151e53dafd0f29417a13f5a30
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2018-07-19 10:16:51 +0100

    Add files forcefully

---
+++ Fanview.API/Model/TeamRanking.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fanview.API.Model
+{
+    public class TeamRanking
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _Id { get; set; }
+        public string TeamId { get; set; }
+        public string TeamRank { get; set; }
+        public string TeamName { get; set; }
+        public int Kill{ get; set; }
+        public float Damage { get; set; }
+        public int TotalPoints { get; set; }        
+        public string MatchId { get; set; }
+    }
+}
