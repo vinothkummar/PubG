f00d0d46cc8996027e99496cf51c698ee54ad15b
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2018-07-20 09:37:07 +0100

    Add the Missing class file to github

---
+++ Fanview.API/Model/ViewModels/PlayerProfileTournament.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fanview.API.Model.ViewModels
+{
+    public class PlayerProfileTournament
+    {
+        public string  MatchId { get; set; }
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public string FullName { get; set; }
+        public string Country { get; set; }
+        public int teamId { get; set; }
+        public Stats stats { get; set; }
+
+    }
+}
