cdeb2faceeeceaca2eab51df23a4c2b29fb9ff03
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2018-07-20 09:54:13 +0100

    Add the missing class file to git hub

---
+++ Fanview.API/Model/ViewModels/Stats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fanview.API.Model.ViewModels
+{
+    public class Stats
+    {
+        public int Knocks { get; set; }
+        public int Assists { get; set; }
+        public int Boosts { get; set; }
+        public float damage{ get; set; }
+        public int headShot { get; set; }
+        public int Heals { get; set; }
+        public int Kills { get; set; }
+        public int TimeSruvived { get; set; }
+    }
+}
