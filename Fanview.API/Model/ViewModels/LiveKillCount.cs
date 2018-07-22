c2507d86d8653c93828beaa6543098c8e8cab04e
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2018-07-22 23:23:00 +0200

    updating missing file

---
+++ Fanview.API/Model/ViewModels/LiveKillCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fanview.API.Model.ViewModels
+{
+    public class LiveKillCount
+    {
+        public int KillerTeamId { get; set; }
+        public string KillerName { get; set; }
+        public int KillCount { get; set; }
+    }
+}
