13b0379176c22821999cdbe43f0e1ef33cd608ec
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2018-07-23 00:27:46 +0200

    updating the mssing file

---
+++ Fanview.API/Model/ViewModels/LiveKillCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fanview.API.Model.ViewModels
+{
+    public class LiveKillCount
+    {
+        public string KillerName { get; set; }
+        public int  KillerTeamId { get; set; }
+        public int KillCount { get; set; }
+    }
+}
