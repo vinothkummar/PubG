b77f82cf36f3f023c3f111cbe75bf2e876a032fd
senthil.sundararaj <senthil.sundararaj@MUG-PC-0034>
2018-07-22 22:34:06 +0100

    Updating the missing file

---
+++ Fanview.API/Model/ViewModels/TesmRankingView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fanview.API.Model.ViewModels
+{
+    public class TeamRankingView
+    {
+        public string TeamId { get; set; }
+        public string TeamRank { get; set; }
+        public string TeamName { get; set; }
+        public int Kill { get; set; }
+        public float Damage { get; set; }
+        public int TotalPoints { get; set; }
+        public int MatchId { get; set; }
+    }
+}
