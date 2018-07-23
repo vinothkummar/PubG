e3c13a8f8264e37266482ecb6b9b3f8177f83242
Manoj <moniee22@hotmail.com>
2018-07-23 01:10:36 -0700

    Adding KillLeader.cs and Kills.cs model

---
+++ Fanview.API/Model/ViewModels/Kills.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fanview.API.Model.ViewModels
+{
+    public class Kills
+    {
+        public string playerName { get; set; }
+        public float playerId { get; set; }
+        public int kills { get; set; }
+        public int timeSurvived { get; set; }
+        public int teamId { get; set; }
+    }
+}
