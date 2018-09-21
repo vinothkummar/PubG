cf300baf2c46d1d85ad6331a8ae21ce541cf5a99
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2018-09-21 12:13:45 +0100

    Added a new feature to record the team eliminated timeout  and improved performance to do the calculation while poling

--- Fanview.API/Model/LiveMatchPlayerStatus.cs
+++ Fanview.API/Model/LiveMatchPlayerStatus.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,8 +8,11 @@ namespace Fanview.API.Model
 {
     public class LiveMatchPlayerStatus
     {
+        [BsonElement("playerId")]
         public int PlayerId { get; set; }
+        [BsonElement("playerName")]
         public string PlayerName { get; set; }
+        [BsonElement("isAlive")]
         public bool IsALive { get; set; }
 
     }
