c9f13f0a375dc925214548ef9e8dfd7162d4cda6
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2018-09-27 14:30:46 +0100

    update missing class file to the git hub

---
+++ Fanview.API/Model/Route.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Fanview.API.Model
+{
+    public class Route
+    {
+        public int TeamID { get; set; }
+        public string TeamRank { get; set; }
+        public string TeamName { get; set; }
+        public string PlayerName { get; set; }
+        public IEnumerable<Location> TeamRoute { get; set; }
+    }
+}
