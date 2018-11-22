77ed4da7a736a3660b61b54c13439ea9544fc72f
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2018-11-22 15:20:17 +0000

    Update the live removing the health

--- Fanview.API/Model/LiveModels/KillZone.cs
+++ Fanview.API/Model/LiveModels/KillZone.cs
@@ -12,7 +12,6 @@ namespace Fanview.API.Model.LiveModels
         public string PlayerName { get; set; }
         public int PlayerId { get; set; }
         public int TeamId { get; set; }
-        public float Health { get; set; }        
         public Location Location { get; set; }
     }
 }
