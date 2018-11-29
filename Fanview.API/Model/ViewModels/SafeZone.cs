59d90fa30118aae3cc6756d40912da8a549621f9
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2018-11-29 11:19:57 +0000

    adding the missing class omitted by git hub

---
+++ Fanview.API/Model/ViewModels/SafeZone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fanview.API.Model.ViewModels
+{
+    public class SafeZone
+    {
+        public int circle;
+
+        public SafetyZonePosition SafetyZonePosition { get; set; }
+       
+        public float SafetyZoneRadius { get; set; }
+    }
+}
