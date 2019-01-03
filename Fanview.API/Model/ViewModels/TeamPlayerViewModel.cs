1a141256e0742eb3992e9d1f4e272e8ff5e15001
Vinoth <Vinoth@DESKTOP-5VDL8CO>
2019-01-03 17:08:30 +0000

    updated the missing the view model

---
+++ Fanview.API/Model/ViewModels/TeamPlayerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fanview.API.Model.ViewModels
+{
+    public class TeamPlayerViewModel
+    { 
+        public string TeamId { get; set; }
+        public int TeamIdShort { get; set; }       
+        public int PlayerId { get; set; }       
+        public string PlayerName { get; set; }        
+        public string FullName { get; set; }        
+        public string Country { get; set; }
+    }
+}
