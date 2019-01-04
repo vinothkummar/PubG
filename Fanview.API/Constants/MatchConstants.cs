702aad136c70f1934b10e408f594c9edb51809e4
steferino <stedidit@gmail.com>
2019-01-04 13:48:27 +0100

    manually added file to git

---
+++ Fanview.API/Constants/MatchConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Fanview.API.Constants
+{
+    public static class MatchConstants
+    {
+        public static readonly int PointsPerKill = 16;
+        public static readonly int[] PlacementPointsTable = new int[]
+        {
+            0, 10, 20, 30, 40, 60, 80, 100, 120, 150, 180, 210, 240, 280, 330, 400
+        };
+    }
+}
