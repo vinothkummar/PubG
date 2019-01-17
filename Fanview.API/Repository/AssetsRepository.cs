364884bd88eca9cd585ccb66c132cd6ac3e87739
steferino <stedidit@gmail.com>
2019-01-17 15:26:10 +0100

    refactored assets repository and added unit test

--- Fanview.API/Repository/AssetsRepository.cs
+++ Fanview.API/Repository/AssetsRepository.cs
@@ -1,66 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Fanview.API.Repository.Interface;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
-using Fanview.API.Assets;
-using Fanview.API.Services.Interface;
+using Fanview.API.Utility;
+using Newtonsoft.Json;
 
 namespace Fanview.API.Repository
 {
     public class AssetsRepository : IAssetsRepository
     {
-        private IHostingEnvironment _hostingEnvironment;
         private ILogger<AssetsRepository> _logger;
-        private ICacheService _cacheService;
 
-
-        public AssetsRepository(IHostingEnvironment hostingEnvironment, ILogger<AssetsRepository> logger, ICacheService cacheService)
+        public AssetsRepository(ILogger<AssetsRepository> logger)
         {
-            _hostingEnvironment = hostingEnvironment;
-            _cacheService = cacheService;
             _logger = logger;
-
-
         }      
 
         public string GetDamageCauserName(string damageCauserKey)
         {
-            var provider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
-
-            var contents = provider.GetDirectoryContents(string.Empty).Where(cn => cn.Name == "Assets").FirstOrDefault().PhysicalPath;
-
-            var fileName = contents + "\\DamageCauserName.json";
-
-            //var fileName = Path.GetFullPath(@"../" + "Fanview.API\\Assets\\DamageCauserName.json");
-
-            JObject damageCauserNameList = JObject.Parse(File.ReadAllText(fileName));
-
-            //var damageCauserNameList = new DamageCauserName(_cacheService).GetDamageCauserName().Result;
-
+            const string damageCauserResourcePath = "Fanview.API.Assets.DamageCauserName.json";
             try
             {
-                var damagedCaused = damageCauserNameList.SelectToken(damageCauserKey);
-
-                //var damagedCaused = damageCauserNameList.FirstOrDefault(cn => cn.Key == damageCauserKey).Value;
-
-                return damagedCaused.ToString();
+                var damageCausersJson = EmbeddedResourcesUtility.ReadEmbeddedResource(damageCauserResourcePath);
+                var damageCausersDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(damageCausersJson);
+                return damageCausersDict[damageCauserKey].ToString();
             }
-            catch (Exception exception)
+            catch (FileNotFoundException)
             {
-                _logger.LogInformation("Damage Causer key " + exception + Environment.NewLine);
-
-                return null;
+                _logger.LogError($"Resource path: {damageCauserResourcePath} not found");
             }
-
-
-
-
+            catch (JsonReaderException)
+            {
+                _logger.LogError($"Failed to deserialize Damage Causers json into a dictionary.");
+            }
+            catch (KeyNotFoundException)
+            {
+                _logger.LogError($"Key {damageCauserKey} not found in the damage causers dictionary.");
+            }
+            return string.Empty;
         }
     }
 }
---
+++ Fanview.Test/Repositories/AssetsRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using Fanview.API.Repository;
+using Fanview.API.Repository.Interface;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fanview.Test.Repositories
+{
+    [TestFixture]
+    class AssetsRepositoryTests
+    {
+        private IAssetsRepository _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+            var logger = Substitute.For<ILogger<AssetsRepository>>();
+            _sut = new AssetsRepository(logger);
+        }
+
+        [TestCase("AquaRail_A_01_C", "Aquarail")]
+        [TestCase("Dacia_A_02_v2_C", "Dacia")]
+        public void GetDamageCauserName_WhenProvidingKey_ShouldReturnExpectedValue(string key, string expectedValue)
+        {
+            // act
+            var res = _sut.GetDamageCauserName(key);
+
+            // assert
+            Assert.IsNotNull(res);
+            Assert.AreEqual(expectedValue, res);
+        }
+
+        [Test]
+        public void GetDamageCauserName_WhenProvidingInvalidKey_ShouldReturnEmptyString()
+        {
+            // act
+            var res = _sut.GetDamageCauserName("nonexistingkey");
+
+            // assert
+            Assert.IsNotNull(res);
+            Assert.AreEqual(string.Empty, res);
+        }
+    }
+}
