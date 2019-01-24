37fc13d16949d2ee4c05d87c47b7ef958a9c62cd
steferino <stedidit@gmail.com>
2019-01-24 12:50:27 +0100

    live damage list doesn't require a match id

--- Fanview.API/Repository/Interface/ILiveRepository.cs
+++ Fanview.API/Repository/Interface/ILiveRepository.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Fanview.API.Model.LiveModels;
 
 namespace Fanview.API.Repository.Interface
 {
     public interface ILiveRepository
     { 
-        Task<LiveDamageList> GetLiveDamageList(string matchId);
+        Task<LiveDamageList> GetLiveDamageList();
     }
 }
