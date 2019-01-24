f7b9fc8dab49c357566033480879ec3165d0b4de
steferino <stedidit@gmail.com>
2019-01-24 13:35:10 +0100

    remove teamrank property from damagelist

--- Fanview.API/Model/LiveModels/DamageList.cs
+++ Fanview.API/Model/LiveModels/DamageList.cs
@@ -1,19 +1,11 @@
-﻿using System;
-
-namespace Fanview.API.Model.LiveModels
+﻿namespace Fanview.API.Model.LiveModels
 {
     public class DamageList
     {
-        //public string MatchName { get; set; }
-        //public int MatchID { get; set; }
-        public double PlayerRank { get; set; }
         public string PlayerName { get; set; }
         public string TeamName { get; set; }
         public int PlayerId { get; set; }
-        //public int Kills { get; set; }
         public double DamageDealt { get; set; }
         public int TeamId { get; set; }
-        //public int TimeSurvived { get; set; }
-
     }
 }
